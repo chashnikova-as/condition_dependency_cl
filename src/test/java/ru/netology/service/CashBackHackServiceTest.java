7ae86fc7026fa52e320a42c76f0dc9bd30f21e08
Chashnikova Anna <xenixgirl@gmail.com>
2020-03-29 23:30:54 +0300

    увеличение покрытия + параметризация

--- src/test/java/ru/netology/service/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -1,28 +1,28 @@
 package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashBackHackServiceTest {
     // amount - сумма покупки, expected - ожидаемый остаток для получения кэшбэка, actual - расчетный остаток до пулучения кэшбека
-    @Test
-    void CheckCashBackFrom990() {
-        int amount = 990;
-        CashBackHackService cashBackHackService = new CashBackHackService();
 
-        int expected = 10;
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackWithoutExceptions.csv", delimiter = '|')
+    void CheckCashBackWithoutExceptions(int amount, int expected, String message) {
+        CashBackHackService cashBackHackService = new CashBackHackService();
         int actual = cashBackHackService.remain(amount);
         assertEquals(expected, actual, "Cashback calculation error!");
     }
 
-    @Test
-    void CheckCashBackFrom1000() {
-        int amount = 1000;
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackFromZero.csv")
+    void CheckCashBackFromZero(int amount) {
         CashBackHackService cashBackHackService = new CashBackHackService();
 
-        int expected = 0;
-        int actual = cashBackHackService.remain(amount);
-        assertEquals(expected, actual, "Cashback calculation error!");
+        assertThrows(IllegalArgumentException.class, () -> { cashBackHackService.remain(amount);
+        });
     }
 }
